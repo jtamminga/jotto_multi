857072e30cf386152e7458ea62e9b76a08c146ea
jtamminga <john.h.tamminga@gmail.com>
2022-04-20 00:28:26 -0400

    trying new display page

--- src/screens/role_select.tsx
+++ src/screens/role_select.tsx
@@ -4,7 +4,12 @@ import { gameFlow } from 'src/core/di'
 export function RoleSelect() {
   return (
     <Screen title="Join game or host" canLeave={false}>
-      <div className="flex flex-col space-y-3">
+
+      <div className="my-10 text-center">
+        a simple <span className="text-emerald-500">five letter</span> word game that is played with a group
+      </div>
+
+      <div className="flex flex-col space-y-3 grow">
 
         {/* join game */}
         <Button
@@ -19,6 +24,11 @@ export function RoleSelect() {
           onClick={() => gameFlow.hostLobby()}
         />
 
+      </div>
+
+      <div className="mb-3 text-center text-slate-400">
+        learn how to play
+        <Button text="tutorial" className="block w-full" type="link" onClick={() => {}} />
       </div>
     </Screen>
   )
